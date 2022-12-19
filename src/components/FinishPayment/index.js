6e56a729f0b5fa87456520d2ff25f2bf76abd564
belleomoraes <iolliveira.moraes@gmail.com>
2022-12-19 15:16:52 -0300

    feat: ticket summary

--- src/components/FinishPayment/index.js
+++ src/components/FinishPayment/index.js
@@ -1,22 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState, useContext } from 'react';
 import UnavailablePayment from './UnavailablePayment';
-import AvailablePayment from './AvailablePayment';
 import useTicket from '../../hooks/api/useTicket';
+import ChoosenTicket from './ChoosenTicket';
 
 export default function FinishPaymentScreen() {
   const { ticket } = useTicket();
-  const [ticketData, setTicketData] = useState([]);
+
+  const [ticketData, setTicketData] = useState({});
 
   useEffect(() => {
     if (ticket) {
       setTicketData({
         ticketName: ticket.TicketType.name,
         ticketPrice: ticket.TicketType.price,
-        status: ticket.TicketType.status
+        status: ticket.TicketType.status,
+        includesHotel: ticket.TicketType.includesHotel,
+        isRemote: ticket.TicketType.isRemote,
       });
     }
-  }, []);
+  }, [ticket]);
 
-  return <>{ticketData.length === 0 ? <UnavailablePayment /> : <AvailablePayment />}</>;
+  return <>{ticketData.length === 0 ? <UnavailablePayment /> : <ChoosenTicket ticketData={ticketData} />}</>;
 }
