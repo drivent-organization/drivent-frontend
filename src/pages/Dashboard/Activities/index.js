92dd9d9bb087f5c0a86a392109edd0f4ba5220ff
belleomoraes <iolliveira.moraes@gmail.com>
2023-01-04 09:31:17 -0300

    feat: show message if ticket hasnt been paid or if ticket is remote

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,30 @@
+import useTicket from '../../../hooks/api/useTicket';
+import { useEffect, useState } from 'react';
+import ActivityScreen from '../../../components/ActivitiesDatas/ActivityScreen';
+import RemoteTicketAlert from '../../../components/ActivitiesDatas/ActivityScreen';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  const [isPaid, setIsPaid] = useState(false);
+  const [isRemote, setIsRemote] = useState(false);
+  const [hasTicket, setHasTicket] = useState();
+  const { ticket } = useTicket();
+  useEffect(() => {
+    if (ticket) {
+      setHasTicket(ticket);
+      if (ticket.status === 'PAID') {
+        setIsPaid(true);
+      }
+
+      if (ticket.TicketType.isRemote) {
+        setIsRemote(true);
+      }
+    }
+  }, [ticket]);
+
+  return (
+    <>
+      {!isRemote && <ActivityScreen isPaid={isPaid} />}
+      {isPaid && isRemote && <RemoteTicketAlert />}
+    </>
+  );
 }
